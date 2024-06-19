4a1b48dab2160d88d3e94a8edcceee7acf87dd33
Carson <32146101+carson4896@users.noreply.github.com>
2024-06-19 17:18:19 -0400

    Completed JavaScript exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    let non_century = year % 4;
+    let century = year % 100;
+    let fourhundred = year % 400;
 
+    if (fourhundred === 0) return true;
+
+    if (century === 0) return false;
+
+    if (non_century === 0) return true;
+        else return false
 };
 
 // Do not edit below this line
